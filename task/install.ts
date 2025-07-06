0495748989c5dfb33d3aa8f75c029b96de4b9775
Chiri Vulpes <chiri@chiri.works>
2025-07-06 21:00:04 +1200

    Port install script over to new functionality provided by task dependency

--- task/install.ts
+++ task/install.ts
@@ -1,18 +1,6 @@
-import ansi from 'ansicolor'
-import fs from 'fs'
-import { Log, Task } from 'task'
+import { Task } from 'task'
 
-interface Package {
-	path: string
-	dependencies: Record<string, PackageDependency>
-}
-
-interface PackageDependency {
-	path: string
-	branch?: string
-}
-
-const packages: Package[] = [
+export default Task('install', async task => task.install(
 	{
 		path: '.',
 		dependencies: {
@@ -28,49 +16,6 @@ const packages: Package[] = [
 			kitsui: { path: 'fluff4me/kitsui', branch: 'package' },
 		},
 	},
-]
-
-export default Task('install', async task => {
-	const root = process.cwd()
-
-	for (const pakige of packages) {
-		process.chdir(root)
-		process.chdir(pakige.path)
-
-		const packageJsonString = await fs.promises.readFile('./package.json', 'utf8')
-
-		const toUpdate = Object.entries(pakige.dependencies)
-
-		const packageListString = toUpdate.map(([name]) => ansi.lightCyan(name)).join(', ')
-		Log.info(`Fetching latest versions of ${packageListString}...`)
-		const toInstall: [name: string, path: string, sha: string][] = await Promise.all(toUpdate.map(async ([name, { path, branch }]) => {
-			let response = ''
-			const branchArg = branch ? `refs/heads/${branch}` : 'HEAD'
-			await task.exec({ stdout: data => response += data.toString() }, 'PATH:git', 'ls-remote', `https://github.com/${path}.git`, branchArg)
-			const sha = response.trim().split(/\s+/)[0]
-			if (!sha)
-				throw new Error(`Failed to get SHA of latest commit of ${name} repository`)
-
-			return [name, path, sha]
-		}))
-
-		Log.info(`Uninstalling ${packageListString}...`)
-		await task.exec('NPM:PATH:npm', 'uninstall', ...toUpdate.map(([name]) => name), '--save', '--no-audit', '--no-fund')
-
-		Log.info(`Installing ${toInstall.map(([name, , sha]) => ansi.lightCyan(`${name}#${sha.slice(0, 7)}`)).join(', ')}...`)
-		await task.exec('NPM:PATH:npm', 'install',
-			...toInstall.map(([name, path, sha]) => `github:${path}#${sha}`),
-			'--save-dev', '--no-audit', '--no-fund'
-		)
-
-		await fs.promises.writeFile('./package.json', packageJsonString, 'utf8')
-	}
-
-	process.chdir(root)
-	process.chdir('src/service')
-	await task.exec('NPM:PATH:npm', 'install', '--no-audit', '--no-fund')
-	process.chdir('../client')
-	await task.exec('NPM:PATH:npm', 'install', '--no-audit', '--no-fund')
-	process.chdir('../platform')
-	await task.exec('NPM:PATH:npm', 'install', '--no-audit', '--no-fund')
-})
+	{ path: 'src/client' },
+	{ path: 'src/service' },
+))
