c1a21ca0a5f907f9b2178ce81a441f27d46e3a97
Chiri Vulpes <chiri@chiri.works>
2025-07-13 15:40:35 +1200

    Skip copying .env if it doesn't exist (ie in build)

--- task/static.ts
+++ task/static.ts
@@ -6,7 +6,7 @@ export default Task('static', async task => {
 	await fs.copyFile('src/platform/index.html', 'out/service/index.html')
 	await fs.mkdir('out/service/service', { recursive: true })
 	await fs.copyFile('src/platform/service.html', 'out/service/service/index.html')
-	await fs.copyFile('.env', 'out/service/.env')
+	await fs.copyFile('.env', 'out/service/.env').catch(() => { })
 	await fs.copyFile('src/platform/node_modules/kitsui/index.js', 'out/service/kitsui.js')
 	await fs.rm('out/service/static', { recursive: true, force: true })
 	await fs.cp('src/platform/static', 'out/service/static', { recursive: true, force: true })
