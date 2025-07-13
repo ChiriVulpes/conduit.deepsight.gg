e153b3b1cb9ce3a68409cef13bbfd8d39f4859f0
Chiri Vulpes <chiri@chiri.works>
2025-07-13 15:42:00 +1200

    Ensure directories exist before placing the amd.js file in them

--- task/vendor.ts
+++ task/vendor.ts
@@ -3,6 +3,8 @@ import { Dependencies, Task } from 'task'
 
 export default Task('vendor', async () => {
 	const amdHeader = await Dependencies.get('amd')
+	await fs.mkdir('src/service/bundle', { recursive: true })
+	await fs.mkdir('src/platform/src/bundle', { recursive: true })
 	await fs.writeFile('src/service/bundle/amd.js', amdHeader)
 	await fs.writeFile('src/platform/src/bundle/amd.js', amdHeader)
 })
