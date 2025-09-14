48a7588389b0ede90561a7a128029137052e2cb2
ChiriVulpes <github@chiri.works>
2025-09-14 12:22:20 +0000

    Deploy 088aeba: Fix not supporting updated Weaving quilt types

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,234 @@
 declare module "conduit.deepsight.gg/Profile" {
+    import type { BungieMembershipType, DestinyClass, DestinyDisplayPropertiesDefinition } from 'bungie-api-ts/destiny2';
     export interface Profile {
         id: string;
+        type: BungieMembershipType;
         name: string;
-        code: `${bigint}`;
+        code?: number;
+        authed?: true;
+        guardianRank?: ProfileGuardianRank;
+        power: number;
+        characters: ProfileCharacter[];
+        emblem?: ProfileEmblem;
+        classType?: DestinyClass;
+        clan?: ProfileClan;
+        lastUpdate: string;
+        lastAccess: string;
+        version: string;
     }
+    export interface ProfileCharacter {
+        id: string;
+        classType: DestinyClass;
+        emblem?: ProfileEmblem;
+        power: number;
+        lastPlayed: string;
+    }
+    export interface ProfileEmblem {
+        hash: number;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+        background: number;
+        secondaryIcon: string;
+        secondaryOverlay: string;
+        secondarySpecial: string;
+    }
+    export interface ProfileClan {
+        name: string;
+        callsign: string;
+    }
+    export interface ProfileGuardianRank {
+        rank: number;
+        name: string;
+    }
+}
+declare module "conduit.deepsight.gg/DefinitionComponents" {
+    import type { AllDestinyManifestComponents, DestinyManifestComponentName } from 'bungie-api-ts/destiny2';
+    import type { ClarityDescription } from 'conduit.deepsight.gg/Clarity';
+    import type { DeepsightManifestComponentsMap } from 'deepsight.gg/Interfaces';
+    export type DeepsightManifestComponentName = keyof DeepsightManifestComponentsMap;
+    export interface AllClarityManifestComponents {
+        ClarityDescriptions: Record<number, ClarityDescription>;
+    }
+    export type ClarityManifestComponentName = keyof AllClarityManifestComponents;
+    export type AllComponentNames = DestinyManifestComponentName | DeepsightManifestComponentName | ClarityManifestComponentName;
+    export type DefinitionsForComponentName<NAME extends AllComponentNames> = (NAME extends DestinyManifestComponentName ? AllDestinyManifestComponents[NAME] : NAME extends DeepsightManifestComponentName ? DeepsightManifestComponentsMap[NAME] : NAME extends ClarityManifestComponentName ? AllClarityManifestComponents[NAME] : never);
 }
 declare module "conduit.deepsight.gg/ConduitMessageRegistry" {
     import type { AuthState, CustomBungieApp } from 'conduit.deepsight.gg/Auth';
+    import type Collections from 'conduit.deepsight.gg/Collections';
+    import type { AllComponentNames, DefinitionsForComponentName } from 'conduit.deepsight.gg/DefinitionComponents';
     import type { Profile } from 'conduit.deepsight.gg/Profile';
     export interface ConduitFunctionRegistry {
-        getProfiles(): Profile[];
-        /** @deprecated This function is for internal use and won't work otherwise */
-        _getAuthState(): AuthState;
-        /** @deprecated This function is for internal use and won't work otherwise */
-        _setCustomApp(app?: CustomBungieApp): void;
-        /** @deprecated This function is for internal use and won't work otherwise */
-        _authenticate(code: string): boolean;
-        /** @deprecated This function is for internal use and won't work otherwise */
-        _grantAccess(origin: string, appName?: string): void;
-        /** @deprecated This function is for internal use and won't work otherwise */
-        _denyAccess(origin: string): void;
+        getProfiles(): Promise<Profile[]>;
+        updateProfiles(): Promise<void>;
+        getProfile(displayName: string, displayNameCode: number): Promise<Profile | undefined>;
+        bumpProfile(displayName: string, displayNameCode: number): Promise<void>;
+        getCollections(): Promise<Collections>;
     }
     export interface ConduitBroadcastRegistry {
-        testBroadcast: string;
+        ready: void;
+        profilesUpdated: Profile[];
+    }
+}
+declare module "conduit.deepsight.gg/Collections" {
+    import type { DestinyAmmunitionType, DestinyClass, DestinyDamageTypeDefinition, DestinyDisplayPropertiesDefinition, DestinyEquipableItemSetDefinition, DestinySandboxPerkDefinition, DestinyStatDefinition, DestinyStatGroupDefinition } from 'bungie-api-ts/destiny2';
+    import type { DeepsightPlugFullName } from 'deepsight.gg/DeepsightPlugCategorisation';
+    import type { ActivityHashes, DamageTypeHashes, EquipableItemSetHashes, InventoryBucketHashes, ItemCategoryHashes, ItemTierTypeHashes, SandboxPerkHashes, StatHashes } from 'deepsight.gg/Enums';
+    import type { ClarityDescription, DeepsightDropTableDefinition, DeepsightItemSourceDefinition, DeepsightItemSourceType, DeepsightMomentDefinition, DeepsightTierTypeDefinition } from 'deepsight.gg/Interfaces';
+    interface Collections {
+        moments: CollectionsMoment[];
+        items: Record<number, Item>;
+        plugs: Record<number, ItemPlug>;
+        rarities: Record<ItemTierTypeHashes, DeepsightTierTypeDefinition>;
+        damageTypes: Record<DamageTypeHashes, DestinyDamageTypeDefinition>;
+        stats: Record<StatHashes, DestinyStatDefinition>;
+        statGroups: Record<number, DestinyStatGroupDefinition>;
+        ammoTypes: Record<DestinyAmmunitionType.Primary | DestinyAmmunitionType.Special | DestinyAmmunitionType.Heavy, ItemAmmo>;
+        itemSets: Record<EquipableItemSetHashes, DestinyEquipableItemSetDefinition>;
+        perks: Partial<Record<SandboxPerkHashes, DestinySandboxPerkDefinition>>;
+        sources: Record<DeepsightItemSourceType, DeepsightItemSourceDefinition>;
+        dropTables: Record<string, DeepsightDropTableDefinition>;
+    }
+    export default Collections;
+    export interface CollectionsMoment {
+        moment: DeepsightMomentDefinition;
+        buckets: {
+            [InventoryBucketHashes.KineticWeapons]: CollectionsBucket;
+            [InventoryBucketHashes.EnergyWeapons]: CollectionsBucket;
+            [InventoryBucketHashes.PowerWeapons]: CollectionsBucket;
+            [InventoryBucketHashes.Helmet]: CollectionsBucket;
+            [InventoryBucketHashes.Gauntlets]: CollectionsBucket;
+            [InventoryBucketHashes.ChestArmor]: CollectionsBucket;
+            [InventoryBucketHashes.LegArmor]: CollectionsBucket;
+            [InventoryBucketHashes.ClassArmor]: CollectionsBucket;
+        };
+    }
+    export interface CollectionsBucket {
+        items: number[];
+    }
+    export interface Item {
+        is: 'item';
+        hash: number;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+        watermark: string;
+        featuredWatermark?: string;
+        sockets: ItemSocket[];
+        type: string;
+        rarity: ItemTierTypeHashes;
+        class?: DestinyClass;
+        damageTypes?: DamageTypeHashes[];
+        ammo?: DestinyAmmunitionType.Primary | DestinyAmmunitionType.Special | DestinyAmmunitionType.Heavy;
+        statGroupHash?: number;
+        stats?: Partial<Record<StatHashes, ItemStat>>;
+        itemSetHash?: EquipableItemSetHashes;
+        flavorText?: string;
+        sources?: ItemSource[];
+        previewImage?: string;
+        foundryImage?: string;
+        categories?: ItemCategoryHashes[];
+        instanceId?: string;
+        tier?: number;
+    }
+    export interface ItemAmmo {
+        hash: DestinyAmmunitionType;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+    }
+    export interface ItemArchetype {
+        hash: number;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+    }
+    export interface ItemSocket {
+        type: DeepsightPlugFullName;
+        plugs: number[];
+        defaultPlugHash?: number;
+    }
+    export interface ItemPlug {
+        is: 'plug';
+        hash: number;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+        type: DeepsightPlugFullName;
+        enhanced: boolean;
+        clarity?: ClarityDescription;
+        perks?: SandboxPerkHashes[];
+        stats?: Partial<Record<StatHashes, ItemStat>>;
+    }
+    export interface ItemStat {
+        hash: StatHashes;
+        value: number;
+        max?: number;
+        displayAsNumeric?: true;
+        intrinsic: number;
+        roll: number;
+        masterwork: number;
+        mod: number;
+        subclass: number;
+        charge: number | number[];
+    }
+    export interface ItemSourceDefined {
+        type: 'defined';
+        id: DeepsightItemSourceType;
+    }
+    export interface ItemSourceDropTable {
+        type: 'table';
+        id: ActivityHashes;
+    }
+    export type ItemSource = ItemSourceDefined | ItemSourceDropTable;
+}
+declare module "conduit.deepsight.gg/Clarity" {
+    export interface ClarityDescriptionTextComponent {
+        text: string;
+        title?: string | ClarityDescriptionComponent[];
+        linesContent?: undefined;
+        table?: undefined;
+        classNames?: string[];
+        formula?: string;
+    }
+    export interface ClarityDescriptionLineComponent {
+        text?: undefined;
+        title?: string | ClarityDescriptionComponent[];
+        linesContent: ClarityDescriptionComponent[];
+        table?: undefined;
+        classNames?: string[];
+        formula?: undefined;
+    }
+    export interface ClarityDescriptionTableCell {
+        cellContent: string | ClarityDescriptionComponent[];
+        classNames?: string[];
+        title?: string | ClarityDescriptionComponent[];
+    }
+    export interface ClarityDescriptionTableRow {
+        rowContent: ClarityDescriptionTableCell[];
+        classNames?: string[];
+        title?: string | ClarityDescriptionComponent[];
+    }
+    export interface ClarityDescriptionTableComponent {
+        text?: undefined;
+        linesContent?: undefined;
+        table: ClarityDescriptionTableRow[];
+        classNames?: string[];
+        isFormula: boolean;
+        formula?: undefined;
+        title?: undefined;
+    }
+    export interface ClarityDescriptionStatValues {
+        stat?: number[];
+        multiplier?: number[];
+    }
+    export interface ClarityDescriptionStat {
+        active?: ClarityDescriptionStatValues;
+        passive?: ClarityDescriptionStatValues;
+        weaponTypes?: string[];
+    }
+    export type ClarityDescriptionComponent = ClarityDescriptionTextComponent | ClarityDescriptionLineComponent | ClarityDescriptionTableComponent;
+    export interface ClarityDescription {
+        hash: number;
+        name: string;
+        itemHash?: number;
+        itemName?: string;
+        lastUpload: number;
+        stats?: Record<string, ClarityDescriptionStat[]>;
+        type: string;
+        uploadedBy: string;
+        descriptions: Record<string, string | ClarityDescriptionComponent[]>;
     }
 }
 declare module "conduit.deepsight.gg/Auth" {
@@ -43,6 +249,20 @@ declare module "conduit.deepsight.gg/Auth" {
         customApp?: CustomBungieApp;
     }
 }
+declare module "conduit.deepsight.gg/Definitions" {
+    import type Conduit from "conduit.deepsight.gg/Conduit";
+    import type { AllComponentNames, DefinitionsForComponentName } from 'conduit.deepsight.gg/DefinitionComponents';
+    interface DefinitionsProvider<DEFINITION> {
+        all(): Promise<DEFINITION>;
+        get(hash?: number | string): Promise<DEFINITION[keyof DEFINITION] | undefined>;
+    }
+    type DefinitionsForLanguage = {
+        [NAME in AllComponentNames]: DefinitionsProvider<DefinitionsForComponentName<NAME>>;
+    };
+    type Definitions = Record<string, DefinitionsForLanguage>;
+    function Definitions(conduit: Conduit): Definitions;
+    export default Definitions;
+}
 declare module "conduit.deepsight.gg/Inventory" {
     namespace Inventory {
         function test(): void;
@@ -50,7 +270,8 @@ declare module "conduit.deepsight.gg/Inventory" {
     export default Inventory;
 }
 declare module "conduit.deepsight.gg" {
-    import type { ConduitFunctionRegistry } from 'conduit.deepsight.gg/ConduitMessageRegistry';
+    import type { ConduitBroadcastRegistry, ConduitFunctionRegistry } from 'conduit.deepsight.gg/ConduitMessageRegistry';
+    import Definitions from "conduit.deepsight.gg/Definitions";
     export { default as Inventory } from "conduit.deepsight.gg/Inventory";
     interface ConduitOptions {
         service?: string;
@@ -60,14 +281,16 @@ declare module "conduit.deepsight.gg" {
             height?: number;
         };
     }
-    type ConduitFunctions = {
-        [KEY in keyof ConduitFunctionRegistry]: (...params: Parameters<ConduitFunctionRegistry[KEY]>) => Promise<ReturnType<ConduitFunctionRegistry[KEY]>>;
-    };
+    export type Unsubscribe = () => void;
     interface ConduitImplementation {
+        on: {
+            [TYPE in keyof ConduitBroadcastRegistry]: (handler: (data: ConduitBroadcastRegistry[TYPE]) => unknown) => Unsubscribe;
+        };
+        readonly definitions: Definitions;
         update(): Promise<void>;
         ensureAuthenticated(appName?: string): Promise<boolean>;
     }
-    interface Conduit extends Omit<ConduitFunctions, keyof ConduitImplementation>, ConduitImplementation {
+    interface Conduit extends ConduitFunctionRegistry, ConduitImplementation {
     }
     function Conduit(options: ConduitOptions): Promise<Conduit>;
     export default Conduit;
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+define("conduit.deepsight.gg/Definitions", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function Definitions(conduit) {
+        return new Proxy({}, {
+            get(target, languageName) {
+                return target[languageName] ??= new Proxy({}, {
+                    get(target, componentName) {
+                        return target[componentName] ??= {
+                            async all() {
+                                return await conduit._getDefinitionsComponent(languageName, componentName);
+                            },
+                            async get(hash) {
+                                return !hash ? undefined : await conduit._getDefinition(languageName, componentName, hash);
+                            },
+                        };
+                    },
+                });
+            },
+        });
+    }
+    exports.default = Definitions;
+});
 define("conduit.deepsight.gg/Inventory", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -12,10 +35,11 @@ define("conduit.deepsight.gg/Inventory", ["require", "exports"], function (requi
     })(Inventory || (Inventory = {}));
     exports.default = Inventory;
 });
-define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inventory"], function (require, exports, Inventory_1) {
+define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Definitions", "conduit.deepsight.gg/Inventory"], function (require, exports, Definitions_1, Inventory_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.Inventory = void 0;
+    Definitions_1 = __importDefault(Definitions_1);
     Object.defineProperty(exports, "Inventory", { enumerable: true, get: function () { return __importDefault(Inventory_1).default; } });
     if (!('serviceWorker' in navigator))
         throw new Error('Service Worker is not supported in this browser');
@@ -30,10 +54,10 @@ define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inve
         document.body.appendChild(iframe);
         await new Promise(resolve => iframe.addEventListener('load', resolve, { once: true }));
         const messageListeners = [];
-        function addListener(type, callback, once = false) {
+        function addListener(id, type, callback, once = false) {
             const expiry = !once ? undefined : Date.now() + 1000 * 60 * 2; // 2 minute expiry for once listeners
             const listener = {
-                id: Math.random().toString(36).slice(2),
+                id,
                 type,
                 callback,
                 once: once ? true : undefined,
@@ -48,20 +72,25 @@ define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inve
                 messageListeners.splice(index, 1);
         }
         function addPromiseListener(type) {
-            return new Promise((resolve, reject) => {
-                const resolveListener = addListener(`resolve:${type}`, data => {
-                    resolve(data);
-                    removeListener(rejectListener.id);
-                }, true);
-                const rejectListener = addListener(`reject:${type}`, data => {
-                    reject(data instanceof Error ? data : new Error('Promise message rejected', { cause: data }));
-                    removeListener(resolveListener.id);
-                }, true);
-            });
+            const id = Math.random().toString(36).slice(2);
+            return {
+                id,
+                promise: new Promise((resolve, reject) => {
+                    addListener(id, `resolve:${type}`, data => {
+                        resolve(data);
+                        removeListener(id);
+                    }, true);
+                    addListener(id, `reject:${type}`, data => {
+                        reject(data instanceof Error ? data : new Error('Promise message rejected', { cause: data }));
+                        removeListener(id);
+                    }, true);
+                }),
+            };
         }
         function callPromiseFunction(type, ...params) {
-            iframe.contentWindow?.postMessage({ type, data: params }, serviceOrigin);
-            return addPromiseListener(type);
+            const { id, promise } = addPromiseListener(type);
+            iframe.contentWindow?.postMessage({ type, id, data: params }, serviceOrigin);
+            return promise;
         }
         let setActive;
         const activePromise = new Promise(resolve => setActive = resolve);
@@ -80,7 +109,7 @@ define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inve
             let used = false;
             for (let i = 0; i < messageListeners.length; i++) {
                 const listener = messageListeners[i];
-                if (listener.type === data.type) {
+                if (listener.type === data.type && listener.id === data.id) {
                     listener.callback(data.data);
                     used = true;
                     if (listener.once) {
@@ -100,6 +129,19 @@ define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inve
         });
         await activePromise;
         const implementation = {
+            definitions: undefined,
+            on: new Proxy({}, {
+                get(target, eventName) {
+                    return (handler) => {
+                        addListener('global', eventName, handler);
+                        return () => {
+                            const index = messageListeners.findIndex(listener => listener.type === 'global' && listener.type === eventName && listener.callback === handler);
+                            if (index !== -1)
+                                messageListeners.splice(index, 1);
+                        };
+                    };
+                },
+            }),
             async update() {
                 return callPromiseFunction('_update');
             },
@@ -142,7 +184,7 @@ define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inve
                 return authState.authenticated && authState.accessGrants.some(grant => grant.origin === window.origin);
             },
         };
-        return new Proxy(implementation, {
+        const conduit = new Proxy(implementation, {
             get(target, fname) {
                 if (fname === 'then')
                     return undefined;
@@ -151,6 +193,8 @@ define("conduit.deepsight.gg", ["require", "exports", "conduit.deepsight.gg/Inve
                 return (...params) => callPromiseFunction(fname, ...params);
             },
         });
+        Object.assign(conduit, { definitions: (0, Definitions_1.default)(conduit) });
+        return conduit;
     }
     exports.default = Conduit;
 });
