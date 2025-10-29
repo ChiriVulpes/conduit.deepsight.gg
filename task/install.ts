4873c5296dabd7c92dc009c9d0d9a0686f006123
Chiri Vulpes <chiri@chiri.works>
2025-10-29 17:31:35 +1300

    Fix ts bundling being bad

--- task/install.ts
+++ task/install.ts
@@ -1,40 +1,45 @@
+import fs from 'fs/promises'
 import { Task } from 'task'
 
-export default Task('install', async task => task.install(
-	{
-		path: '.',
-		dependencies: {
-			task: { repo: 'chirivulpes/task', branch: 'package' },
-			lint: { repo: 'fluff4me/lint' },
-			chiri: { repo: 'fluff4me/chiri', branch: 'package' },
-			weaving: { repo: 'chirivulpes/weaving', branch: 'package' },
+export default Task('install', async task => {
+	await task.install(
+		{
+			path: '.',
+			dependencies: {
+				task: { repo: 'chirivulpes/task', branch: 'package' },
+				lint: { repo: 'fluff4me/lint' },
+				chiri: { repo: 'fluff4me/chiri', branch: 'package' },
+				weaving: { repo: 'chirivulpes/weaving', branch: 'package' },
+			},
 		},
-	},
-	{
-		path: 'src/platform',
-		dependencies: {
-			kitsui: { repo: 'fluff4me/kitsui', branch: 'package' },
+		{
+			path: 'src/platform',
+			dependencies: {
+				kitsui: { repo: 'fluff4me/kitsui', branch: 'package' },
+			},
 		},
-	},
-	{
-		path: 'src/client',
-		dependencies: {
-			'deepsight.gg': { name: 'deepsight.gg' },
-			'bungie-api-ts': { name: 'bungie-api-ts' },
+		{
+			path: 'src/client',
+			dependencies: {
+				'deepsight.gg': { name: 'deepsight.gg' },
+				'bungie-api-ts': { name: 'bungie-api-ts' },
+			},
 		},
-	},
-	{
-		path: 'src/service',
-		dependencies: {
-			'deepsight.gg': { name: 'deepsight.gg' },
-			'bungie-api-ts': { name: 'bungie-api-ts' },
+		{
+			path: 'src/service',
+			dependencies: {
+				'deepsight.gg': { name: 'deepsight.gg' },
+				'bungie-api-ts': { name: 'bungie-api-ts' },
+			},
 		},
-	},
-	{
-		path: 'src/shared',
-		dependencies: {
-			'deepsight.gg': { name: 'deepsight.gg' },
-			'bungie-api-ts': { name: 'bungie-api-ts' },
+		{
+			path: 'src/shared',
+			dependencies: {
+				'deepsight.gg': { name: 'deepsight.gg' },
+				'bungie-api-ts': { name: 'bungie-api-ts' },
+			},
 		},
-	},
-))
+	)
+
+	await fs.rename('src/service/node_modules/jsonpath-plus/dist/index-browser-umd.min.cjs', 'src/service/node_modules/jsonpath-plus/dist/index-browser-umd.min.js').catch(() => { })
+})
