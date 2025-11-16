775e02e26dffffbb2ca1d6ba564813170e644ff8
Chiri Vulpes <chiri@chiri.works>
2025-11-17 12:37:42 +1300

    Cachebust manifest fetching

--- src/service/model/ClarityManifest.ts
+++ src/service/model/ClarityManifest.ts
@@ -8,7 +8,7 @@ interface ClarityManifest {
 export default Model('ClarityManifest', {
 	cacheDirtyTime: 1000 * 60 * 60, // 1 hour cache time
 	async fetch () {
-		const manifest = await Clarity.get<ClarityManifest>('/versions.json')
+		const manifest = await Clarity.get<ClarityManifest>(`/versions.json?_=${Math.random().toString(36).slice(2)}`)
 		if (typeof manifest?.descriptions !== 'number')
 			throw new Error('Invalid Destiny manifest response')
 
--- src/service/model/DeepsightManifest.ts
+++ src/service/model/DeepsightManifest.ts
@@ -5,7 +5,7 @@ import Deepsight from 'utility/Deepsight'
 const DeepsightManifest = Model('DeepsightManifest', {
 	cacheDirtyTime: 1000 * 60 * 1, // 1 minute cache time
 	async fetch () {
-		const manifestPromise = Deepsight.get<DeepsightManifest>('/manifest.json')
+		const manifestPromise = Deepsight.get<DeepsightManifest>(`/manifest.json?_=${Math.random().toString(36).slice(2)}`)
 		const manifest = await manifestPromise
 		if (typeof manifest?.deepsight !== 'number')
 			throw new Error('Invalid Destiny manifest response')
