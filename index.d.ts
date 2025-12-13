c4b47127373e0dcdaa70d7fc1544d3c9be5bc6ca
ChiriVulpes <github@chiri.works>
2025-12-13 22:01:06 +0000

    Deploy 979e462: Rework collections/items stuff so inventory stuff can reuse a lot of it

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,112 @@
+declare module "conduit.deepsight.gg/item/Item" {
+    import type { DestinyAmmunitionType, DestinyClass, DestinyDamageTypeDefinition, DestinyDisplayPropertiesDefinition, DestinyEquipableItemSetDefinition, DestinySandboxPerkDefinition, DestinySocketCategoryDefinition, DestinyStatDefinition, DestinyStatGroupDefinition } from 'bungie-api-ts/destiny2';
+    import type { DeepsightPlugFullName } from 'deepsight.gg/DeepsightPlugCategorisation';
+    import type { ActivityHashes, DamageTypeHashes, EquipableItemSetHashes, FoundryHashes, ItemCategoryHashes, ItemTierTypeHashes, MomentHashes, SandboxPerkHashes, SocketCategoryHashes, StatHashes } from 'deepsight.gg/Enums';
+    import type { ClarityDescription, DeepsightDropTableDefinition, DeepsightItemSourceDefinition, DeepsightItemSourceType, DeepsightTierTypeDefinition, DeepsightWeaponFoundryDefinition } from 'deepsight.gg/Interfaces';
+    export interface ItemProvider {
+        items: Record<number, Item>;
+        plugs: Record<number, ItemPlug>;
+        rarities: Record<ItemTierTypeHashes, DeepsightTierTypeDefinition>;
+        damageTypes: Record<DamageTypeHashes, DestinyDamageTypeDefinition>;
+        stats: Record<StatHashes, DestinyStatDefinition>;
+        statGroups: Record<number, DestinyStatGroupDefinition>;
+        ammoTypes: Record<DestinyAmmunitionType.Primary | DestinyAmmunitionType.Special | DestinyAmmunitionType.Heavy, ItemAmmo>;
+        itemSets: Record<EquipableItemSetHashes, DestinyEquipableItemSetDefinition>;
+        perks: Partial<Record<SandboxPerkHashes, DestinySandboxPerkDefinition>>;
+        sources: Record<DeepsightItemSourceType, DeepsightItemSourceDefinition>;
+        dropTables: Record<ActivityHashes, DeepsightDropTableDefinition>;
+        socketCategories: Record<SocketCategoryHashes, DestinySocketCategoryDefinition>;
+        foundries: Record<FoundryHashes, DeepsightWeaponFoundryDefinition>;
+    }
+    export interface Item {
+        is: 'item';
+        hash: number;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+        momentHash?: MomentHashes;
+        featured: boolean;
+        sockets: ItemSocket[];
+        type: string;
+        rarity: ItemTierTypeHashes;
+        classType?: DestinyClass;
+        damageTypeHashes?: DamageTypeHashes[];
+        ammoType?: DestinyAmmunitionType.Primary | DestinyAmmunitionType.Special | DestinyAmmunitionType.Heavy;
+        statGroupHash?: number;
+        stats?: Partial<Record<StatHashes, ItemStat>>;
+        itemSetHash?: EquipableItemSetHashes;
+        flavorText?: string;
+        sources?: ItemSource[];
+        previewImage?: string;
+        foundryHash?: FoundryHashes;
+        categoryHashes?: ItemCategoryHashes[];
+        instanceId?: string;
+        tier?: number;
+    }
+    export interface ItemAmmo {
+        hash: DestinyAmmunitionType;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+    }
+    export interface ItemSocket {
+        type: DeepsightPlugFullName;
+        plugs: number[];
+        defaultPlugHash?: number;
+    }
+    export interface ItemPlug {
+        is: 'plug';
+        hash: number;
+        displayProperties: DestinyDisplayPropertiesDefinition;
+        type: DeepsightPlugFullName;
+        enhanced: boolean;
+        clarity?: ClarityDescription;
+        perks?: SandboxPerkHashes[];
+        stats?: Partial<Record<StatHashes, ItemStat>>;
+    }
+    export interface ItemStat {
+        hash: StatHashes;
+        value: number;
+        max?: number;
+        displayAsNumeric?: true;
+        intrinsic: number;
+        roll: number;
+        masterwork: number;
+        mod: number;
+        subclass: number;
+        charge: number | number[];
+    }
+    export interface ItemSourceDefined {
+        type: 'defined';
+        id: DeepsightItemSourceType;
+    }
+    export interface ItemSourceDropTable {
+        type: 'table';
+        id: ActivityHashes;
+    }
+    export type ItemSource = ItemSourceDefined | ItemSourceDropTable;
+}
+declare module "conduit.deepsight.gg/item/Collections" {
+    import type { InventoryBucketHashes } from 'deepsight.gg/Enums';
+    import type { DeepsightMomentDefinition } from 'deepsight.gg/Interfaces';
+    import type { ItemProvider } from 'conduit.deepsight.gg/item/Item';
+    interface Collections extends ItemProvider {
+        moments: CollectionsMoment[];
+    }
+    export default Collections;
+    export interface CollectionsMoment {
+        moment: DeepsightMomentDefinition;
+        buckets: {
+            [InventoryBucketHashes.KineticWeapons]: CollectionsBucket;
+            [InventoryBucketHashes.EnergyWeapons]: CollectionsBucket;
+            [InventoryBucketHashes.PowerWeapons]: CollectionsBucket;
+            [InventoryBucketHashes.Helmet]: CollectionsBucket;
+            [InventoryBucketHashes.Gauntlets]: CollectionsBucket;
+            [InventoryBucketHashes.ChestArmor]: CollectionsBucket;
+            [InventoryBucketHashes.LegArmor]: CollectionsBucket;
+            [InventoryBucketHashes.ClassArmor]: CollectionsBucket;
+        };
+    }
+    export interface CollectionsBucket {
+        items: number[];
+    }
+}
 declare module "conduit.deepsight.gg/Settings" {
     export interface ConduitSettings {
         verboseLogging: true | undefined;
@@ -112,9 +221,9 @@ declare module "conduit.deepsight.gg/ConduitState" {
 }
 declare module "conduit.deepsight.gg/ConduitMessageRegistry" {
     import type { AuthState, CustomBungieApp } from 'conduit.deepsight.gg/Auth';
-    import type Collections from 'conduit.deepsight.gg/Collections';
     import type ConduitState from 'conduit.deepsight.gg/ConduitState';
     import type { AllComponentNames, DefinitionLinks, DefinitionReferencesPage, DefinitionsFilter, DefinitionsForComponentName, DefinitionsPage, DefinitionWithLinks } from 'conduit.deepsight.gg/DefinitionComponents';
+    import type Collections from 'conduit.deepsight.gg/item/Collections';
     import type { Profile } from 'conduit.deepsight.gg/Profile';
     import type { ConduitSettings } from 'conduit.deepsight.gg/Settings';
     export interface ConduitFunctionRegistry {
@@ -139,108 +248,6 @@ declare module "conduit.deepsight.gg/ConduitMessageRegistry" {
         profilesUpdated: Profile[];
         _updateSettings: void;
     }
-}
-declare module "conduit.deepsight.gg/Collections" {
-    import type { DestinyAmmunitionType, DestinyClass, DestinyDamageTypeDefinition, DestinyDisplayPropertiesDefinition, DestinyEquipableItemSetDefinition, DestinySandboxPerkDefinition, DestinySocketCategoryDefinition, DestinyStatDefinition, DestinyStatGroupDefinition } from 'bungie-api-ts/destiny2';
-    import type { DeepsightPlugFullName } from 'deepsight.gg/DeepsightPlugCategorisation';
-    import type { ActivityHashes, DamageTypeHashes, EquipableItemSetHashes, FoundryHashes, InventoryBucketHashes, ItemCategoryHashes, ItemTierTypeHashes, MomentHashes, SandboxPerkHashes, SocketCategoryHashes, StatHashes } from 'deepsight.gg/Enums';
-    import type { ClarityDescription, DeepsightDropTableDefinition, DeepsightItemSourceDefinition, DeepsightItemSourceType, DeepsightMomentDefinition, DeepsightTierTypeDefinition, DeepsightWeaponFoundryDefinition } from 'deepsight.gg/Interfaces';
-    interface Collections {
-        moments: CollectionsMoment[];
-        items: Record<number, Item>;
-        plugs: Record<number, ItemPlug>;
-        rarities: Record<ItemTierTypeHashes, DeepsightTierTypeDefinition>;
-        damageTypes: Record<DamageTypeHashes, DestinyDamageTypeDefinition>;
-        stats: Record<StatHashes, DestinyStatDefinition>;
-        statGroups: Record<number, DestinyStatGroupDefinition>;
-        ammoTypes: Record<DestinyAmmunitionType.Primary | DestinyAmmunitionType.Special | DestinyAmmunitionType.Heavy, ItemAmmo>;
-        itemSets: Record<EquipableItemSetHashes, DestinyEquipableItemSetDefinition>;
-        perks: Partial<Record<SandboxPerkHashes, DestinySandboxPerkDefinition>>;
-        sources: Record<DeepsightItemSourceType, DeepsightItemSourceDefinition>;
-        dropTables: Record<ActivityHashes, DeepsightDropTableDefinition>;
-        socketCategories: Record<SocketCategoryHashes, DestinySocketCategoryDefinition>;
-        foundries: Record<FoundryHashes, DeepsightWeaponFoundryDefinition>;
-    }
-    export default Collections;
-    export interface CollectionsMoment {
-        moment: DeepsightMomentDefinition;
-        buckets: {
-            [InventoryBucketHashes.KineticWeapons]: CollectionsBucket;
-            [InventoryBucketHashes.EnergyWeapons]: CollectionsBucket;
-            [InventoryBucketHashes.PowerWeapons]: CollectionsBucket;
-            [InventoryBucketHashes.Helmet]: CollectionsBucket;
-            [InventoryBucketHashes.Gauntlets]: CollectionsBucket;
-            [InventoryBucketHashes.ChestArmor]: CollectionsBucket;
-            [InventoryBucketHashes.LegArmor]: CollectionsBucket;
-            [InventoryBucketHashes.ClassArmor]: CollectionsBucket;
-        };
-    }
-    export interface CollectionsBucket {
-        items: number[];
-    }
-    export interface Item {
-        is: 'item';
-        hash: number;
-        displayProperties: DestinyDisplayPropertiesDefinition;
-        momentHash?: MomentHashes;
-        featured: boolean;
-        sockets: ItemSocket[];
-        type: string;
-        rarity: ItemTierTypeHashes;
-        classType?: DestinyClass;
-        damageTypeHashes?: DamageTypeHashes[];
-        ammoType?: DestinyAmmunitionType.Primary | DestinyAmmunitionType.Special | DestinyAmmunitionType.Heavy;
-        statGroupHash?: number;
-        stats?: Partial<Record<StatHashes, ItemStat>>;
-        itemSetHash?: EquipableItemSetHashes;
-        flavorText?: string;
-        sources?: ItemSource[];
-        previewImage?: string;
-        foundryHash?: FoundryHashes;
-        categoryHashes?: ItemCategoryHashes[];
-        instanceId?: string;
-        tier?: number;
-    }
-    export interface ItemAmmo {
-        hash: DestinyAmmunitionType;
-        displayProperties: DestinyDisplayPropertiesDefinition;
-    }
-    export interface ItemSocket {
-        type: DeepsightPlugFullName;
-        plugs: number[];
-        defaultPlugHash?: number;
-    }
-    export interface ItemPlug {
-        is: 'plug';
-        hash: number;
-        displayProperties: DestinyDisplayPropertiesDefinition;
-        type: DeepsightPlugFullName;
-        enhanced: boolean;
-        clarity?: ClarityDescription;
-        perks?: SandboxPerkHashes[];
-        stats?: Partial<Record<StatHashes, ItemStat>>;
-    }
-    export interface ItemStat {
-        hash: StatHashes;
-        value: number;
-        max?: number;
-        displayAsNumeric?: true;
-        intrinsic: number;
-        roll: number;
-        masterwork: number;
-        mod: number;
-        subclass: number;
-        charge: number | number[];
-    }
-    export interface ItemSourceDefined {
-        type: 'defined';
-        id: DeepsightItemSourceType;
-    }
-    export interface ItemSourceDropTable {
-        type: 'table';
-        id: ActivityHashes;
-    }
-    export type ItemSource = ItemSourceDefined | ItemSourceDropTable;
 }
 declare module "conduit.deepsight.gg/Clarity" {
     export interface ClarityDescriptionTextComponent {
